e3da76c6526f9978a3e7dc8497459eddb5ba813f
hoasung01 <nguyenngochai.shipagent@gmail.com>
2015-07-13 12:26:06 +0700

    added attachements stuff in action mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,8 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
+    attachments['filename.jpg'] = {:mime_type => 'application/mymimetype',
+                                   :content => some_string }
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 
